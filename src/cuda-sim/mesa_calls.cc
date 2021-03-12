c4cc2f7ba0fff9caf375145224d49d0c17ac538f
Mohammadreza saed <mohammadrezasaed@gmail.com>
2021-03-12 20:06:44 +0330

    add empty gpgpusim_vkCmdTraceRaysKHR function

--- src/cuda-sim/mesa_calls.cc
+++ src/cuda-sim/mesa_calls.cc
@@ -18,4 +18,18 @@ extern "C" void gpgpusim_testTraversal(struct anv_bvh_node* root)
     //VulkanRayTracing::
 }
 
+extern "C" void gpgpusim_vkCmdTraceRaysKHR(
+                      const VkStridedDeviceAddressRegionKHR *raygen_sbt,
+                      const VkStridedDeviceAddressRegionKHR *miss_sbt,
+                      const VkStridedDeviceAddressRegionKHR *hit_sbt,
+                      const VkStridedDeviceAddressRegionKHR *callable_sbt,
+                      bool is_indirect,
+                      uint32_t launch_width,
+                      uint32_t launch_height,
+                      uint32_t launch_depth,
+                      uint64_t launch_size_addr)
+{
+    printf("calling vkCmdTraceRaysKHR in gpgpusim\n");
+}
+
 #endif /* MESA_CALLS_H */
