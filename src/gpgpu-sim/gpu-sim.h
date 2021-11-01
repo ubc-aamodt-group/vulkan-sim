703cda7123fdddcfaa4fbeb42047c3554b83dfab
Mohammadreza Saed <mohammadrezasaed@gmail.com>
2021-10-31 19:19:06 -0700

    fix gpu power bug

--- src/gpgpu-sim/gpu-sim.h
+++ src/gpgpu-sim/gpu-sim.h
@@ -103,8 +103,10 @@ struct power_config {
     // NOTE: After changing the nonlinear model to only scaling idle core,
     // NOTE: The min_inc_per_active_sm is not used any more
     if (g_use_nonlinear_model)
-      sscanf(gpu_nonlinear_model_config, "%lf:%lf", &gpu_idle_core_power,
-             &gpu_min_inc_per_active_sm);
+      // sscanf(gpu_nonlinear_model_config, "%lf:%lf", &gpu_idle_core_power,
+      //        &gpu_min_inc_per_active_sm);
+      gpu_idle_core_power = 0;
+      gpu_min_inc_per_active_sm = 0;
   }
   void reg_options(class OptionParser *opp);
 
