d10b215d6d74ff82dccf4959ef28fc6166e92428
Mohammadreza Saed <mohammadrezasaed@gmail.com>
2022-04-14 19:20:06 -0700

    add traversal results to store_transactions

--- src/cuda-sim/vulkan_ray_tracing.cc
+++ src/cuda-sim/vulkan_ray_tracing.cc
@@ -743,6 +743,8 @@ void VulkanRayTracing::traceRay(VkAccelerationStructureKHR _topLevelAS,
         float3 barycentric = Barycentric(object_intersection_point, p[0], p[1], p[2]);
         traversal_data.closest_hit.barycentric_coordinates = barycentric;
         thread->RT_thread_data->set_hitAttribute(barycentric);
+
+        store_transactions.push_back(MemoryStoreTransactionRecord(&traversal_data, sizeof(traversal_data), StoreTransactionType::Traversal_Results));
     }
     else
     {
